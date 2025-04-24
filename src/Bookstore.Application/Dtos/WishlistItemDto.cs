7548469de451c218f1aa9e54c58f31a0c156b0da
hoangnguyenn26 <nguyencntt08@gmail.com>
2025-04-24 23:20:55 +0700

    fix: fix wishlistdto name

--- src/Bookstore.Application/Dtos/WishlistItemDto.cs
+++ src/Bookstore.Application/Dtos/WishlistItemDto.cs
@@ -1,11 +1,11 @@
 ﻿
-using Bookstore.Application.Dtos.Books; 
+using Bookstore.Application.Dtos.Books;
 
 namespace Bookstore.Application.Dtos.Wishlists
 {
     public class WishlistItemDto
     {
-        public Guid Id { get; set; } 
+        public Guid Id { get; set; }
         public Guid BookId { get; set; }
         public DateTime CreatedAtUtc { get; set; }
         public BookDto Book { get; set; } = null!;
